a8fa06ae953ba8ecf51be5d83159f7614ca90a17
USMAN HAMEED <usmanham33d@live.com>
2023-06-16 02:38:21 +0800

    feat: 1.0.8

--- index.d.ts
+++ index.d.ts
@@ -3,7 +3,7 @@ import * as THREE from 'three';
 export declare function Scene(scene: THREE.Scene, camera: THREE.Camera): Promise<Blob>;
 
 export declare class AreaLight extends THREE.Object3D {
-    constructor(width: number, height: number, color?: number|THREE.Color, intensity?: number);
+    constructor(width: number, height: number, color?: number, intensity?: number);
 }
 
 export declare const Domain: string;
